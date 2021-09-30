bc27bc628d3aa3f5a7fc4b55aa31f5ce866c6418
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2021-09-30 15:59:00 -0400

    gen-lrat

---
+++ comproof/gen-lrat.sh
@@ -0,0 +1,14 @@
+CNF=$1
+COMPROOF=$2
+
+SIZE=`wc $COMPROOF | awk '{print $1}'`
+
+for (( i=0; i<$SIZE; i++ ))
+do
+  j=$(($i+1))
+  echo $j
+  cat $CNF | awk -v var=$i '/p cnf/ {printf "p cnf %i %i\n", $3, $4+var} / 0/ {print $0}' > tmp.cnf
+  head -n $i $COMPROOF >> tmp.cnf
+  ~/drat-trim/drat-trim tmp.cnf drat/proof-$j.drat -L lrat/proof-$j.lrat
+done
+
