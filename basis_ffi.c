cb1a09d7bb294d53d183760cffb04548e466b618
tanyongkiam <tanyongkiam@gmail.com>
2022-08-09 23:28:48 +0800

    remove bloat in basis_ffi.c

--- basis_ffi.c
+++ basis_ffi.c
@@ -3,30 +3,16 @@
   library, as a thin wrapper around the relevant system calls.
 */
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/mman.h>
 #include <assert.h>
-
-#ifndef __WIN32
-#include <signal.h>
-#endif
 
 /* This flag is on by default. It catches CakeML's out-of-memory exit codes
  * and prints a helpful message to stderr.
  * Note that this is not specified by the basis library.
  * */
 #define STDERR_MEM_EXHAUST
-
-/* This is set to 1 when the runtime traps a SIGINT */
-
-#ifndef __WIN32
-volatile sig_atomic_t caught_sigint = 0;
-#endif
 
 /* clFFI (command line) */
 
@@ -42,31 +28,6 @@ extern void *cml_stackend;
 extern char cake_text_begin;
 extern char cake_codebuffer_begin;
 extern char cake_codebuffer_end;
-
-/* Signal handler for SIGINT */
-
-#ifndef __WIN32
-void do_sigint(int sig_num)
-{
-    signal(SIGINT, do_sigint);
-    caught_sigint = 1;
-}
-#endif
-
-void ffipoll_sigint (unsigned char *c, long clen, unsigned char *a, long alen)
-{
-    if (alen < 1) {
-        return;
-    }
-    a[0] = (unsigned char) caught_sigint;
-    caught_sigint = 0;
-}
-
-void ffikernel_ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
-    for (long i = 0; i < clen; i++) {
-        putc(c[i], stdout);
-    }
-}
 
 void ffiget_arg_count (unsigned char *c, long clen, unsigned char *a, long alen) {
   a[0] = (char) argc;
@@ -267,35 +228,6 @@ void ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
   #endif
 }
 
-typedef union {
-  double d;
-  char bytes[8];
-} double_bytes;
-
-// FFI calls for floating-point parsing
-void ffidouble_fromString (char *c, long clen, char *a, long alen) {
-  double_bytes d;
-  sscanf(c, "%lf",&d.d);
-  assert (8 == alen);
-  for (int i = 0; i < 8; i++){
-    a[i] = d.bytes[i];
-  }
-}
-
-void ffidouble_toString (char *c, long clen, char *a, long alen) {
-  double_bytes d;
-  assert (256 == alen);
-  for (int i = 0; i < 8; i++){
-    d.bytes[i] = a[i];
-  }
-  //snprintf always terminates with a 0 byte if space was sufficient
-  int bytes_written = snprintf(&a[0], 255, "%.20g", d.d);
-  // snprintf returns number of bytes it would have written if the buffer was
-  // large enough -> check that it did not write more than the buffer size - 1
-  // for the 0 byte
-  assert (bytes_written <= 255);
-}
-
 void cml_clear() {
   __builtin___clear_cache(&cake_codebuffer_begin, &cake_codebuffer_end);
 }
@@ -374,28 +306,6 @@ int main (int local_argc, char **local_argv) {
 
   cml_stack = cml_heap + cml_heap_sz;
   cml_stackend = cml_stack + cml_stack_sz;
-
-  /** Set up the "eval" code buffer to be read-write-execute. **/
-  if(mprotect(&cake_text_begin, &cake_codebuffer_end - &cake_text_begin,
-              PROT_READ | PROT_WRITE | PROT_EXEC))
-  {
-    #ifdef STDERR_MEM_EXHAUST
-    fprintf(stderr,"failed to set permissions for CakeML code buffer.\n");
-    perror("mprotect");
-    #endif
-    exit(3);
-  }
-
-  /* Set up the signal handler for SIGINTs when running the REPL. */
-  #ifndef __WIN32
-  for (int i = 0; i < local_argc; i++) {
-      if (strcmp(local_argv[i], "--repl") == 0 ||
-          strcmp(local_argv[i], "--candle") == 0) {
-        signal(SIGINT, do_sigint);
-        break;
-      }
-  }
-  #endif
 
   cml_main(); // Passing control to CakeML
 
